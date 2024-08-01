ffb6d0a01d4b92d6e639b2f31d01eba67365975f
Jerome <jerome.sf@outlook.com>
2024-08-01 15:05:55 +0800

    update README

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # maodou-lang
+本项目基于`https://github.com/miao-lang/miao-lang`，感谢作者的贡献
+
+项目使用vue2
+node版本：14.15
+
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: './',
+  publicPath: './',  // 没有这个github page会找不到静态资源
   outputDir: 'dist', // dist
   assetsDir: 'static',
   transpileDependencies: true
